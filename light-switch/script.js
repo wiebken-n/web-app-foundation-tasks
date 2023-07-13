42cd975c5b7b041d6863a624019a882a0bd1c828
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-07-13 15:43:13 +0200

    Refactored lightswitch toggle function

--- light-switch/script.js
+++ light-switch/script.js
@@ -3,12 +3,10 @@ const html = document.querySelector("html");
 
 button.addEventListener("click", function () {
   if (!button.className.includes("lightswitch-dark")) {
-    button.className += " lightswitch-dark";
-    html.className += " background-dark";
     console.log("light off");
   } else if (button.className.includes("lightswitch-dark")) {
-    button.className = button.className.replace(" lightswitch-dark", "");
-    html.className = html.className.replace(" background-dark", "");
     console.log("light on");
   }
+  button.classList.toggle("lightswitch-dark");
+  html.classList.toggle("background-dark");
 });
