030faf70d45bf01cb2dc0416f1dadc3410d47f1f
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-07-14 18:06:18 +0200

    Feat: Adds conversion of rgb to hex

--- color-mixer/script.js
+++ color-mixer/script.js
@@ -14,4 +14,22 @@ sliderContainer.addEventListener("pointermove", function () {
   html.style.setProperty("--value-red", redValue);
   html.style.setProperty("--value-green", greenValue);
   html.style.setProperty("--value-blue", blueValue);
+
+  let hexColor = "#" + convertToHex(redValue, greenValue, blueValue);
+
+  hexValue.innerText = hexColor;
+
+  function valueConvert(rgbValue) {
+    rgbValue = parseInt(rgbValue);
+    let hex = rgbValue.toString(16);
+    return hex;
+  }
+
+  function convertToHex(r, g, b) {
+    let value = valueConvert(r) + valueConvert(g) + valueConvert(b);
+    while (value.length < 6) {
+      value = "0" + value;
+    }
+    return value;
+  }
 });
