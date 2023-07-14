f26381d87ce06486dcb2fc71d6b789fd0aa4b796
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-07-14 15:38:43 +0200

    Feat: Adds solution for counter-task

---
+++ counter/script.js
@@ -0,0 +1,38 @@
+// initialize variables for elements
+const counterArea = document.querySelector(".counter");
+const counterText = document.querySelector(".count");
+const resetButton = document.querySelector(".reset-button");
+const html = document.querySelector("html");
+// initialize variables for counter numbers
+let count = 0;
+let countShown = 0;
+
+// execute countUp function if bottom area is clicked
+counterArea.addEventListener("click", function () {
+  countUp();
+});
+// execute countUp function if space is used
+document.addEventListener("keyup", function (event) {
+  if (event.code === "Space") {
+    countUp();
+  }
+});
+
+// reset counters and thus reset css-value if reset button is clicked
+resetButton.addEventListener("click", function () {
+  count = 0;
+  countShown = 0;
+  html.style.setProperty("--count-value", count + "%");
+  counterText.innerText = countShown;
+});
+
+// increment counters and adjust css value accordingly
+const countUp = function () {
+  if (count > 99) {
+    count = 0;
+  }
+  count++;
+  countShown++;
+  html.style.setProperty("--count-value", count + "%");
+  counterText.innerText = countShown;
+};
