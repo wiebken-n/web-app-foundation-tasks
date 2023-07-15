f0efe613173c94cdf00478ad0cd76252a182a98e
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-07-15 22:54:36 +0200

    Feat: Adds solution for clock task

---
+++ clock/script.js
@@ -0,0 +1,38 @@
+const html = document.querySelector("html");
+const digiHours = document.querySelector(".digi-hours");
+const digiMinutes = document.querySelector(".digi-minutes");
+const digiSeconds = document.querySelector(".digi-seconds");
+const digiDots = document.querySelectorAll(".digi-dots");
+
+const analogHours = document.querySelector(".analog-hours");
+const analogMinutes = document.querySelector(".analog-minutes");
+const analogSeconds = document.querySelector(".analog-seconds");
+
+function updateTime() {
+  //  get current time
+  let date = new Date();
+
+  // digital clock
+  digiHours.innerText = convertTime(date.getHours());
+  digiMinutes.innerText = convertTime(date.getMinutes());
+  digiSeconds.innerText = convertTime(date.getSeconds());
+  digiDots.forEach((entry) => entry.classList.toggle("invisible"));
+
+  // analog clock
+  html.style.setProperty("--seconds", date.getSeconds() * 6 + "deg");
+  html.style.setProperty("--minutes", date.getMinutes() * 6 + "deg");
+  html.style.setProperty("--hours", (date.getHours() / 2) * 30 + "deg");
+}
+// convert time to double digits (if necessary)
+function convertTime(time) {
+  time = time.toString();
+  if (time.length === 1) {
+    time = "0" + time;
+  }
+  return time;
+}
+
+// execute function
+updateTime();
+// set interval at which current time is updated
+setInterval(updateTime, 1000);
