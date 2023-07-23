923469530ce22907e394361801f3e1fb8089e5ba
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-07-23 11:31:48 +0200

    Fix: Fixes check-functionality

--- password-check/script.js
+++ password-check/script.js
@@ -1,28 +1,27 @@
 const pwInput1 = document.querySelector("#pw-input-1");
 const pwInput2 = document.querySelector("#pw-input-2");
+const pwInputContainer = document.querySelector("#pw-input-container");
 const btnShowPw = document.querySelector("#btn-show-pw");
+
+let pw1 = "";
+let pw2 = "";
 
 btnShowPw.addEventListener("click", function (event) {
   event.preventDefault();
   inputToggle(pwInput1);
   inputToggle(pwInput2);
-  /*switch (pwInput1.type) {
-    case "password":
-      pwInput1.setAttribute("type", "text");
-      break;
-    case "text":
-      pwInput1.setAttribute("type", "password");
-      break;
-  }
-  switch (pwInput2.type) {
-    case "password":
-      pwInput2.setAttribute("type", "text");
-      break;
-    case "text":
-      pwInput2.setAttribute("type", "password");
-      break;
-  }
-  */
+});
+
+pwInputContainer.addEventListener("input", function () {
+  pw1 = pwInput1.value.toString();
+  pw2 = pwInput2.value.toString();
+  pwEqualCheck();
+});
+
+pwInput2.addEventListener("input", function () {
+  pw1 = pwInput1.value.toString();
+  pw2 = pwInput2.value.toString();
+  pwEqualCheck();
 });
 
 function inputToggle(pwInput) {
@@ -35,48 +34,72 @@ function inputToggle(pwInput) {
       break;
   }
 }
-pwInput1.addEventListener("input", function () {
-  pwEqualCheck();
-});
-
-pwInput2.addEventListener("input", function () {
-  pwEqualCheck();
-});
 
 function pwEqualCheck() {
-  switch (pwInput1.value === pwInput2.value) {
+  switch (pw1 === pw2) {
     case true:
       document.querySelector("#pw-equal-indicator").innerText = "✅";
+      console.log("equal true");
       pwLowerCase();
       pwUpperCase();
       pwLengthCheck();
       pwNumberCheck();
       break;
+    case false:
+      console.log("equal false");
+      pwEqualCheckFailRender();
+      break;
     default:
+      console.log("equal default");
       pwEqualCheckFailRender();
       break;
   }
 }
 
 function pwLowerCase() {
-  const lowerCaseCheck = (character) => character === character.toLowerCase();
-  switch (pwInput1.value.split("").some(lowerCaseCheck)) {
+  const lowerCaseCheck = (character) => {
+    if (character === character.toLowerCase() && isNaN(character)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  switch (pw1.split("").some(lowerCaseCheck)) {
     case true:
       document.querySelector("#pw-lower-case-indicator").innerText = "✅";
+      console.log("lower true");
+      break;
+    case false:
+      document.querySelector("#pw-lower-case-indicator").innerText = "❌";
+      console.log("lower true");
       break;
     default:
       document.querySelector("#pw-lower-case-indicator").innerText = "❌";
+      console.log("lower def");
       break;
   }
 }
 
 function pwUpperCase() {
-  const upperCaseCheck = (character) => character === character.toUpperCase();
-  switch (pwInput1.value.split("").some(upperCaseCheck)) {
+  const upperCaseCheck = (character) => {
+    if (character === character.toUpperCase() && isNaN(character)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  switch (pw1.split("").some(upperCaseCheck)) {
     case true:
+      console.log("upper true");
       document.querySelector("#pw-upper-case-indicator").innerText = "✅";
       break;
+    case false:
+      console.log("upper false");
+      document.querySelector("#pw-upper-case-indicator").innerText = "❌";
+      break;
+
     default:
+      console.log("upper false");
       document.querySelector("#pw-upper-case-indicator").innerText = "❌";
       break;
   }
@@ -84,25 +107,46 @@ function pwUpperCase() {
 
 function pwNumberCheck() {
   const numberCheck = function (character) {
-    return !isNaN(character);
+    if (parseInt(character) * 10 >= 10) {
+      return true;
+    } else {
+      return false;
+    }
   };
-  console.log(pwInput1.value.split("").some(numberCheck));
-  switch (pwInput1.value.split("").some(numberCheck)) {
+
+  switch (pw1.split("").some(numberCheck)) {
     case true:
+      console.log("number true");
       document.querySelector("#pw-number-indicator").innerText = "✅";
       break;
+    case false:
+      console.log("number false");
+      document.querySelector("#pw-number-indicator").innerText = "❌";
+      break;
     default:
+      console.log("number default");
       document.querySelector("#pw-number-indicator").innerText = "❌";
       break;
   }
 }
 
 function pwLengthCheck() {
-  switch (pwInput1.value.length >= 10 && pwInput1.value.length >= 10) {
+  switch (
+    pw1.length >= 10 &&
+    pw2.length >= 10 &&
+    pw1.length > 0 &&
+    pw2.length > 0
+  ) {
     case true:
+      console.log("length true");
       document.querySelector("#pw-10-characters-indicator").innerText = "✅";
       break;
+    case false:
+      console.log("length false");
+      document.querySelector("#pw-10-characters-indicator").innerText = "❌";
+      break;
     default:
+      console.log("length default");
       document.querySelector("#pw-10-characters-indicator").innerText = "❌";
       break;
   }
@@ -115,3 +159,19 @@ function pwEqualCheckFailRender() {
   document.querySelector("#pw-number-indicator").innerText = "❌";
   document.querySelector("#pw-10-characters-indicator").innerText = "❌";
 }
+
+/*
+function trueOrFalse(boolean, string) {
+  switch (boolean) {
+    case true:
+      document.querySelector(string).innerText = "✅";
+      break;
+    case false:
+      document.querySelector(string).innerText = "❌";
+      break;
+    default:
+      document.querySelector(string).innerText = "❌";
+      break;
+  }
+}
+*/
